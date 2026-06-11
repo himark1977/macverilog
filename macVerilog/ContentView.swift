676356afce0d5488b9c15ef42411eaa5c2400175
Ilie-Adrian Avramescu <himark1977@protonmail.com>
2026-06-11 17:34:34 +0300

    Initial Commit

---
+++ macVerilog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macVerilog
+//
+//  Created by Ilie-Adrian Avramescu on 11/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macVerilog/macVerilogApp.swift
@@ -0,0 +1,17 @@
+//
+//  macVerilogApp.swift
+//  macVerilog
+//
+//  Created by Ilie-Adrian Avramescu on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct macVerilogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
